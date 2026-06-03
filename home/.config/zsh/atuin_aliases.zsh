193afd70218fa66376cf30112b07d2877209f48f
Xmetalfanx <Xmetalfanx@yahoo.com>
2026-06-03 13:26:39 -0400

    [Nix config related] Added commented out option for allowUnFree
    not sure if i need this later so i left it but commented it out

--- home/.config/zsh/atuin_aliases.zsh
+++ home/.config/zsh/atuin_aliases.zsh
@@ -2,6 +2,7 @@
 alias at="atuin"
 ## Atuin aliases for deleting and pruning history
 alias ats='atuin search'
-alias atddday='atuin search "." --before "1 day ago" --delete'
-alias atdw='atuin search "." --before "7 days ago" --delete'
-alias atdm='atuin search "." --before "30 days ago" --delete'
+# old way 
+#alias atddday='atuin search "." --before "1 day ago" --delete'
+#alias atdw='atuin search "." --before "7 days ago" --delete'
+#alias atdm='atuin search "." --before "30 days ago" --delete'
