8af1a88ec46b7e779d1fac00ac33808128a0c716
Xmetalfanx <Xmetalfanx@yahoo.com>
2026-06-03 13:27:45 -0400

    [Atuin Related] Updated Alias file

--- home/.config/zsh/atuin_aliases.zsh
+++ home/.config/zsh/atuin_aliases.zsh
@@ -1,8 +1,8 @@
 # Atuin aliases
 alias at="atuin"
+
+alias ath="atuin history"
+
 ## Atuin aliases for deleting and pruning history
 alias ats='atuin search'
-# old way 
-#alias atddday='atuin search "." --before "1 day ago" --delete'
-#alias atdw='atuin search "." --before "7 days ago" --delete'
-#alias atdm='atuin search "." --before "30 days ago" --delete'
+
