75df5f613fdadbccfc2d68d9bc9d497505820277
Xmetalfanx <Xmetalfanx@yahoo.com>
2026-06-06 17:29:51 -0400

    [atuin/zsh] Updated atuin aliases

---
+++ home/.config/zsh/aliases_and_functions/atuin_aliases.zsh
@@ -0,0 +1,13 @@
+# Atuin aliases
+alias at="atuin"
+alias ath="atuin history"
+alias ats='atuin search'
+# do i need $ats here ?
+alias atsd='ats --delete'
+
+
+#######################################################
+
+# loading the functions
+echo "Loading Atuin functions"
+. $zsh_aliases_dir/atuin_functions.zsh
--- home/.config/zsh/atuin_aliases.zsh
+++
@@ -1,8 +0,0 @@
-# Atuin aliases
-alias at="atuin"
-
-alias ath="atuin history"
-
-## Atuin aliases for deleting and pruning history
-alias ats='atuin search'
-
